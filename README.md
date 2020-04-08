906344e906d7c81558e48a4c49dbd8a094221587
Jose M. Mencia <josma.mencia@gmail.com>
2020-04-08 16:05:36 +0200

    .

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# BigDataPython
+# BDPython
 
 Material de apoyo del libro BIG DATA con PYTHON. Recolección, almacenamiento y proceso,
 de Enrique Martín Martín, Adrián Riesco y Rafael Caballero, editado por RC libros con
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/jmencia/workspace/python/cursos/BigDataPython/venv"
+set -gx VIRTUAL_ENV "/home/jmencia/workspace/python/cursos/BDPython/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
