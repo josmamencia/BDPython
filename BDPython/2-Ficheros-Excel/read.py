fbeb24500be00af25841b5d45824aabdaedf76aa
Jose M. Mencia <josma.mencia@gmail.com>
2020-04-10 13:49:55 +0200

    .

---
+++ BDPython/2-Ficheros-Excel/read.py
@@ -0,0 +1,19 @@
+from xlrd import open_workbook, colname
+import xlwt
+
+with open_workbook('./data/Cap1/subvenciones.xls', on_demand=True) as libro:
+    asociaciones = {}
+    for nombreHoja in libro.sheet_names():
+        hoja = libro.sheet_by_name(nombreHoja)
+        print('Hoja ' + nombreHoja)
+        for i in range(1, hoja.nrows):
+            fila = hoja.row(i)
+            centro = fila[0].value
+            subvencion = fila[2].value
+            # print(fila[0].ctype)
+            # print(fila[2].value)
+            if centro in asociaciones:
+                asociaciones[centro] = asociaciones[centro] + subvencion
+            else:
+                asociaciones[centro] = subvencion
+    print(asociaciones)
